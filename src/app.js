2533c0c2def73ad927f6193f4a46111de9ec60e3
sam gaylert <sam.gaylert@nyu.edu>
2018-04-12 01:07:15 -0400

    milestone 2 code finished and ready to be put on server

--- src/app.js
+++ src/app.js
@@ -1,48 +1,120 @@
 // app.js
-
-//boiler plate requires and variables
 const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;
+//boiler plate requires and variables
+const path = require('path');
 const publicPath = path.join(__dirname, '/public/');
-const session = require('express-session');
-
-//mongoose integration
-require('./db.js');
 const mongoose = require('mongoose');
+require('./db.js');
 const Review = mongoose.model('Review');
-const Location = mongoose.mpdel('Location');
-const User = mongoose.mpdel('User');
-const sessionOptions = { 
-	secret: 'secret for signing session id', 
-	saveUninitialized: false, 
-	resave: false 
-};
+const Location = mongoose.model('Location');
+const User = mongoose.model('User');
+
+
+const passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+const session = require("express-session")
+	,bodyParser = require("body-parser");
+
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(function(user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+	User.findById(id, function(err, user) {
+		done(err, user);
+	});
+});
+	  
+
+app.use(express.static(publicPath));
+app.use(session({ secret: "itsfreerealestate", resave: false, saveUninitialized: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+	// now you can use {{user}} in your template!
+	if(req.user){
+		res.locals.path = "/review/write";
+		res.locals.username = req.user.username;
+	}
+	else{
+		res.locals.path = "/login";
+	}
+    next();
+});
 
 app.set('view engine', 'hbs');
-app.use(express.static(publicPath));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session(sessionOptions));
-
-
-
 
 /*
 	pathing
 */
+app.get('/register', (req, res) => {
+	res.render('register');
+});
+
+app.post('/register', (req, res) => {
+	const username = req.body.username;
+	const email = req.body.email;
+	User.register(new User({username: username, email:email}), req.body.password, function(err) {
+		if (err) {
+		  return next(err);
+		}
+		passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login'})(req,res);
+	  });
+
+});
+
+app.get('/login', (req,res) => {
+	res.render('login');
+});
+
+app.get('/review/write', (req, res) => {
+	res.render('write',{username: req.user.username});
+});
+
+app.post('/review/write', (req, res) => {
+	if(req.user){
+		const newReview = new Review({
+			location: req.body.location,
+			username: req.user.username,
+			rating: req.body.rating,
+			review: req.body.review
+		});
+		newReview.save(function(err){
+			if(err){ 
+				res.render('write', {message: 'REVIEW ADD ERROR'}); 
+			}
+			else{
+				res.redirect('/');
+			}
+        });
+	}
+});
+
+app.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login'}));
+
+
+
 app.get('/', (req, res)=> {
-	
 	Review.find((err, revs) => {
 		if(err){
 			res.send(err);
 		}
-		res.render('index',{rev: revs});
+		if(req.user){
+			res.render('index',{rev: revs, message: req.user.username});
+		}
+		else{
+			res.render('index',{rev: revs});
+		}
 	});
 });
 
 
-app.listen(PORT);
+app.listen(process.env.PORT || 3000);
 
 
--- src/db.js
+++ src/db.js
@@ -1,19 +1,17 @@
 
 const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
-
+const userSchema = new mongoose.Schema({
+	email: String
+});
+userSchema.plugin(passportLocalMongoose);
 
 const revSchema = new mongoose.Schema({
 	location: String,
-	user: [userSchema],
+	username: String,
 	rating: Number,
 	review: String
-});
-
-const userSchema = new mongoose.Schema({
-	username: { type: String, required: true, unique: true },
-	email: String,
-	hash: { type: String, required: true}
 });
 
 const locationSchema = new mongoose.Schema({
@@ -27,4 +25,18 @@ mongoose.model('User', userSchema);
 mongoose.model('Location', locationSchema);
 
 
-mongoose.connect('mongodb://localhost/final-project');
+
+
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+ const fs = require('fs');
+ const path = require('path');
+ const fn = path.join(__dirname, 'config.json');
+ const data = fs.readFileSync(fn);
+ const conf = JSON.parse(data);
+ dbconf = conf.dbconf;
+} else {
+ dbconf = 'mongodb://localhost/final-project';
+}
+
+mongoose.connect(dbconf);
